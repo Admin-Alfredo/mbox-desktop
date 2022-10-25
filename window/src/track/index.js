991ee6a1963671993136daeddfaf49174c8cb224
Admiro Alfredo <admiroalfredo1742@gmail.com>
2022-10-25 17:37:10 -0300

    2.1 -  adionado a class [Track] in the window/src/track/index.js

--- window/src/track/index.js
+++ window/src/track/index.js
@@ -1,11 +1,3 @@
-// let ext = path.extname(filename)
-// let name = path.basename(filename, ext)
-// let source = path.join(musicDir, filename)
-// let size = statSync(source).size
-// let fullDirname = path.dirname(source)
-// let splitedDirname = fullDirname.split(path.sep)
-// let dirname = splitedDirname[splitedDirname.length - 1]
-// return { File: { ext, name, size }, source, fullDirname, dirname }
 const { statSync } = require('fs')
 const path = require('path')
 class Track{
@@ -13,9 +5,11 @@ class Track{
     const ext = path.extname(filename)
     const name = path.basename(filename, ext)
     this.source = path.join(directory, filename)
-    const size = statSync(this.source)
-    this.File = { name, ext, size }
-    const directorySplited = path.dirname(source).split(path.sep)
+    const stat = statSync(this.source)
+    this.File = { name, ext, stat }
+    const directorySplited = path.dirname(this.source).split(path.sep)
     this.dirname = directorySplited[directorySplited.length - 1]
+    this.fullDirname = path.dirname(this.source)
   }
 }
+module.exports = Track
